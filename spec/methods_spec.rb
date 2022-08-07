f237387022be605bc37d0fbea40d6fb679cf9934
Christian TDN <cjwtdn@gmail.com>
2022-08-07 13:32:48 -0600

    commit tree 6531391c3c197a39e435ec4227137ad26e4219b6

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -52,3 +52,34 @@ describe '#halve' do
   end
 
 end
+
+
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+greet ("Jimmy")
+
+def greet_with_default (name="programmer")
+  puts "Hello, #{name}!"
+end
+greet_with_default ("Naureen")
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+add(3,5)
+
+def halve(num1)
+if num1.class !=Integer
+  return nil
+end
+  num1/2
+end
+
+halve(6)
